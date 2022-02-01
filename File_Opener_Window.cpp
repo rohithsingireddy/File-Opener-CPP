8e07227c2d89d03955162959627d2ae8c168c086
Rohith <rohithdevprofile@gmail.com>
2022-02-01 12:08:44 +0530

    Added a search bar that forward searches for first word

--- File_Opener_Window.cpp
+++ File_Opener_Window.cpp
@@ -9,7 +9,12 @@ File_Opener_Window::File_Opener_Window(
     : Gtk::ApplicationWindow(cobject),
       m_refBuilder(refBuilder),
       m_stack(nullptr),
-      m_gears(nullptr)
+      m_gears(nullptr),
+      m_settings(),
+      m_search(nullptr),
+      m_searchbar(nullptr),
+      m_searchentry(nullptr),
+      m_prop_binding()
 {
     m_stack = m_refBuilder->get_widget<Gtk::Stack>("stack");
     if (!m_stack)
@@ -18,22 +23,55 @@ File_Opener_Window::File_Opener_Window(
     }
 
     m_gears = m_refBuilder->get_widget<Gtk::MenuButton>("gears");
-    if( !m_gears )
+    if (!m_gears)
     {
         throw std::runtime_error("No \"gears\" object in window.ui");
     }
 
-    m_settings = Gio::Settings::create("org.mt.fileopener");
-    m_settings->bind("transition", m_stack->property_transition_type());
+    m_search = m_refBuilder->get_widget<Gtk::ToggleButton>("search");
+    if (!m_search)
+    {
+        throw std::runtime_error("No \"search\" object in window.ui");
+    }
+
+    m_searchentry = m_refBuilder->get_widget<Gtk::SearchEntry>("searchentry");
+    if (!m_searchentry)
+    {
+        throw std::runtime_error("No \"searchentry\" object in window.ui");
+    }
+
+    m_searchbar = m_refBuilder->get_widget<Gtk::SearchBar>("searchbar");
+    if (!m_searchbar)
+    {
+        throw std::runtime_error("No \"searchbar\" object in window.ui");
+    }
 
     auto menu_builder = Gtk::Builder::create_from_resource("/org/mt/fileopener/gears_menu.ui");
     auto menu = menu_builder->get_object<Gio::MenuModel>("menu");
-    if(!menu)
+    if (!menu)
     {
         throw std::runtime_error("No \"menu\" object in gears_menu.ui");
     }
-
     m_gears->set_menu_model(menu);
+
+    m_prop_binding = Glib::Binding::bind_property(
+        m_search->property_active(),
+        m_searchbar->property_search_mode_enabled(),
+        Glib::Binding::Flags::BIDIRECTIONAL);
+
+    m_searchentry->signal_search_changed().connect(
+        sigc::mem_fun(
+            *this,
+            &File_Opener_Window::on_search_text_changed));
+
+    m_stack->property_visible_child().signal_changed().connect(
+        sigc::mem_fun(
+            *this,
+            &File_Opener_Window::on_visible_child_changed));
+
+    m_settings = Gio::Settings::create("org.mt.fileopener");
+    m_settings->bind("transition", m_stack->property_transition_type());
+
 }
 
 File_Opener_Window *File_Opener_Window::create()
@@ -51,18 +89,18 @@ File_Opener_Window *File_Opener_Window::create()
 void File_Opener_Window::open_file_view(const Glib::RefPtr<Gio::File> &file)
 {
     const Glib::ustring basename = file->get_basename();
-    
+
     auto view = Gtk::make_managed<Gtk::TextView>();
     view->set_editable(false);
     view->set_cursor_visible(false);
     view->set_margin(5);
-    
+
     auto scrolled = Gtk::make_managed<Gtk::ScrolledWindow>();
     scrolled->set_expand(true);
     scrolled->set_child(*view);
     m_stack->add(*scrolled, basename, basename);
 
-    try 
+    try
     {
         char *contents = nullptr;
         gsize length = 0;
@@ -70,18 +108,62 @@ void File_Opener_Window::open_file_view(const Glib::RefPtr<Gio::File> &file)
         view->get_buffer()->set_text(contents, contents + length);
         g_free(contents);
     }
-    catch(const Glib::Error &er)
+    catch (const Glib::Error &er)
     {
         std::cerr << "File_Opener_Window::open_file_view(\"" << file->get_parse_name()
-                    << "\"): " << er.what() << std::endl;
+                  << "\"): " << er.what() << std::endl;
     }
-    
+
     auto buffer = view->get_buffer();
     auto tag = buffer->create_tag();
     m_settings->bind("font", tag->property_font());
     buffer->apply_tag(
         tag,
         buffer->begin(),
-        buffer->end()
-    );
+        buffer->end());
+    
+    m_search->set_sensitive(true);
+}
+
+void File_Opener_Window::on_search_text_changed()
+{
+    const auto text = m_searchentry->get_text();
+    if (text.empty())
+    {
+        return;
+    }
+
+    auto tab = dynamic_cast<Gtk::ScrolledWindow *>(m_stack->get_visible_child());
+    if (!tab)
+    {
+        std::cerr << "File_Opener_Window::on_search_text_changed(): No visible child" << std::endl;
+        return;
+    }
+
+    auto view = dynamic_cast<Gtk::TextView *>(tab->get_child());
+    if (!view)
+    {
+        std::cerr << "File_Opener_Window::on_search_text_changed(): No visible text view. " << std::endl;
+        return;
+    }
+
+    // Can be done with a suffix tree
+    auto buffer = view->get_buffer();
+    Gtk::TextIter match_start, match_end;
+    if (buffer->begin().forward_search(
+            text,
+            Gtk::TextSearchFlags::CASE_INSENSITIVE,
+            match_start,
+            match_end))
+    {
+        buffer->select_range(match_start, match_end);
+        view->scroll_to(match_start);
+
+    }
+
+}
+
+void File_Opener_Window::on_visible_child_changed()
+{
+    m_searchbar->set_search_mode(false);
 }
--- File_Opener_Window.h
+++ File_Opener_Window.h
@@ -1,11 +1,16 @@
 #ifndef GTK_FILE_OPENER_WINDOW_GUARD
 #define GTK_FILE_OPENER_WINDOW_GUARD
 
+#include <iostream>
 #include <gtkmm/applicationwindow.h>
 #include <gtkmm/builder.h>
 #include <gtkmm/stack.h>
 #include <gtkmm/menubutton.h>
 #include <giomm/settings.h>
+#include <gtkmm/searchbar.h>
+#include <gtkmm/togglebutton.h>
+#include <gtkmm/searchentry.h>
+#include <glibmm/binding.h>
 
 class File_Opener_Window : public Gtk::ApplicationWindow
 {
@@ -18,10 +23,18 @@ public:
     void open_file_view(const Glib::RefPtr<Gio::File> &file);
 
 protected:
+    void on_search_text_changed();
+    void on_visible_child_changed();
+    
     Glib::RefPtr<Gtk::Builder> m_refBuilder;
     Glib::RefPtr<Gio::Settings> m_settings;
+    Glib::RefPtr<Glib::Binding> m_prop_binding;
     Gtk::Stack *m_stack;
     Gtk::MenuButton *m_gears;
+    Gtk::ToggleButton *m_search;
+    Gtk::SearchBar *m_searchbar;
+    Gtk::SearchEntry *m_searchentry;
+    
 };
 
 #endif /* GTK_FILE_OPENER_WINDOW_GUARD */
